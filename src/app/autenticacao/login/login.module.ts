8562bbfd2318d05c986bcfed86eca5ab52468bd9
deivison <dguarines@gmail.com>
2019-05-05 21:02:13 -0300

    Criando a tela de login - parte 1/2

--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatListModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSnackBarModule
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { LoginComponent, LogarComponent } from './components';
 
@@ -11,7 +24,16 @@ import { LoginComponent, LogarComponent } from './components';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule
   ]
 })
 
