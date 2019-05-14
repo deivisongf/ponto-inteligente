0cc802564d63fc17ba28bb655a28e0d7b639092b
Deivison <dguarines@gmail.com>
2019-05-14 13:18:18 -0300

    Criando o módulo e componente de cadastro de pessoa jurídica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { LoginModule, LoginRoutingModule } from './autenticacao';
+import { LoginModule,
+         LoginRoutingModule,
+         CadastroPjModule } from './autenticacao';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -18,6 +20,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatButtonModule,
     LoginModule,
     LoginRoutingModule,
+    CadastroPjModule,
     
     AppRoutingModule
   ],
