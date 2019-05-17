86217000d2788f93b2dbcdb61da06804d8b740d7
Deivison <dguarines@gmail.com>
2019-05-17 14:00:47 -0300

    Criando o formulário de cadastro de pessoa física - Parte 1

--- src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
+++ src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
@@ -2,6 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatListModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatSnackBarModule
+} from '@angular/material';
+
+import { SharedModule } from '../../shared/shared.module';
 
 import { 
 	CadastrarPfComponent, 
@@ -12,7 +25,16 @@ import {
   imports: [
     CommonModule,
     FlexLayoutModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    SharedModule
   ],
   declarations: [
   	CadastrarPfComponent,
--- src/app/autenticacao/cadastro-pf/components/cadastrar-pf/cadastrar-pf.component.ts
+++ src/app/autenticacao/cadastro-pf/components/cadastrar-pf/cadastrar-pf.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+
+import {
+  CpfValidator,
+  CnpjValidator
+} from '../../../../shared/validators';
 
 @Component({
   selector: 'app-cadastrar-pf',
   templateUrl: './cadastrar-pf.component.html',
   styleUrls: ['./cadastrar-pf.component.css']
 })
+
 export class CadastrarPfComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.gerarForm();
+  }
+
+  gerarForm(){
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha:['', [Validators.required, Validators.minLength(6)]],
+      cpf:['', [Validators.required, CpfValidator]],
+      cnpj:['', [Validators.required, CnpjValidator]]
+    });
   }
 
 }
