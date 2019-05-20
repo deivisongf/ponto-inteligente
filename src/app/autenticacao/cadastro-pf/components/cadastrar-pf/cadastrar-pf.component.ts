51d0766311731b29fcc79233f672f8dee9bff439
Deivison <dguarines@gmail.com>
2019-05-20 12:57:58 -0300

    Criando o model de cadastro de pessoa física

--- src/app/autenticacao/cadastro-pf/components/cadastrar-pf/cadastrar-pf.component.ts
+++ src/app/autenticacao/cadastro-pf/components/cadastrar-pf/cadastrar-pf.component.ts
@@ -8,6 +8,8 @@ import {
   CnpjValidator
 } from '../../../../shared/validators';
 
+import { CadastroPf } from '../../models';
+
 @Component({
   selector: 'app-cadastrar-pf',
   templateUrl: './cadastrar-pf.component.html',
@@ -43,7 +45,9 @@ export class CadastrarPfComponent implements OnInit {
       return;
     }
 
-    alert(JSON.stringify(this.form.value));
+    const cadastroPf: CadastroPf = this.form.value;
+
+    alert(JSON.stringify(cadastroPf));
 
     return false;
   }
