f4e2df89ce465823874f0c10498705371f4d505d
Deivison <dguarines@gmail.com>
2019-05-23 14:03:20 -0300

    Implementando o componente de lançamento - parte 1/2

--- src/app/funcionario/components/lancamento/lancamento.component.ts
+++ src/app/funcionario/components/lancamento/lancamento.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+
+import {
+  Tipo
+} from '../../../shared';
+
+import * as moment from 'moment';
+
+declare var navigator: any;
 
 @Component({
   selector: 'app-lancamento',
@@ -7,9 +17,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoComponent implements OnInit {
 
-  constructor() { }
+  private dataAtualEn: string;
+  dataAtual: string;
+  geoLocation: string;
+  ultimoTipoLancado: string;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private router : Router
+  ) { }
 
   ngOnInit() {
+      this.dataAtual = moment().format('DD/MM/YYYY HH:mm:ss');
+      this.dataAtualEn = moment().format('YYYY-MM-DD HH:mm:ss');
+      this.obterGeolocation();
+      this.ultimoTipoLancado = '';
+      this.obterUltimoLancamento();
+  }
+
+  obterGeolocation(): string {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position => this.geoLocation = `${position.coords.latitude},${position.coords.longitude}`);
+    }
+
+    return '';
+  }
+
+  iniciarTrabalho(){
+    this.cadastrar(Tipo.INICIO_TRABALHO);
+  }
+
+  terminarTrabalho(){
+    this.cadastrar(Tipo.TERMINO_TRABALHO);
+  }
+
+  iniciarAlmoco(){
+    this.cadastrar(Tipo.INICIO_ALMOCO);
+  }
+
+  terminarAlmoco(){
+    this.cadastrar(Tipo.TERMINO_ALMOCO);
+  }
+
+  obterUltimoLancamento(){
+    this.ultimoTipoLancado = '';
+  }
+
+  cadastrar(tipo: Tipo){
+    alert();
   }
 
 }
--- src/app/funcionario/funcionario.module.ts
+++ src/app/funcionario/funcionario.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
- 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatListModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatCardModule
+} from '@angular/material';
+
 import  { 
   ListagemComponent,
   LancamentoComponent,
@@ -18,7 +32,18 @@ import  {
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule
   ]
 })
 
