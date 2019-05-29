9d59fa395db68028a452b6559f6d901496dda757
Deivison <dguarines@gmail.com>
2019-05-29 13:01:37 -0300

    Criando a listagem de lançamentos

--- src/app/shared/services/lancamento.service.ts
+++ src/app/shared/services/lancamento.service.ts
@@ -35,6 +35,23 @@ export class LancamentoService {
   	);
   }
 
+  listarTodosLancamentos(): Observable<any> {
+    return this.http.get(
+        env.baseApiUrl + this.PATH + 
+          this.PATH_TODOS_LANC.replace(
+            '{funcionarioId}', this.httpUtil.obterIdUsuario()),
+        this.httpUtil.headers()
+    );
+  }
+
 }
+
+
+
+
+
+
+
+
 
 
