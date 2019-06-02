a9d6445f89ece0bcc67cde5377cd8a9e7b64e7df
deivison <dguarines@gmail.com>
2019-06-02 13:00:03 -0300

    Criando a listagem de lançamentos (Lazy loading datatable)

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatListModule,
+  MatTooltipModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatSelectModule,
+  MatRadioModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MAT_DATE_LOCALE,
+  MatDialogModule,
+  MatPaginatorModule,
+  MatPaginatorIntl,
+  MatSortModule
+} from '@angular/material';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { 
+  HttpUtilService, 
+  LancamentoService,
+  PtBrMatPaginatorIntl 
+} from '../shared';
 
 import { 
   ListagemComponent, 
@@ -21,7 +48,30 @@ import {
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    MatSortModule,
+    SharedModule
+  ],
+  providers:[
+    LancamentoService,
+    HttpUtilService,
+    MatPaginatorIntl,
+    { provide: MAT_DATE_LOCALE, useValue:'pt-BR' },
+    { provide: MatPaginatorIntl, useClass: PtBrMatPaginatorIntl }
   ]
 })
 export class AdminModule { }
